3333fd1323e76f86e1492a5dd7228bd6420cfdc7
DESKTOP-H17UMK4\goku <ssingh239@sapient.com>
2017-07-18 00:02:00 +0530

    webpack demoo lazy load

---
+++ training/webpack-demo/web-pack.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};/**
+ * Created by goku on 17-07-2017.
+ */
