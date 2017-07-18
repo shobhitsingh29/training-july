e3e49fcd5a4533f2436874e376e91ec8bc2ebb9b
shobhit <shobhitsingh29@gmail.com>
2017-07-18 14:43:56 +0530

    webpack  duplicate

--- training/webpack-demo/src/index.js
+++ training/webpack-demo/src/index.js
@@ -1,11 +1,34 @@
-import _ from 'lodash';
-function component() {
-    var element = document.createElement('div');
+/*import _ from 'lodash';*/
+function getComponent() {
+  /*  var element = document.createElement('div');
+    var button = document.createElement('button');
+    var br = document.createElement('br');
+    button.innerHTML = 'Click me and look at the console!';
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');*/
+  /*  element.appendChild(br);
+    element.appendChild(button);*/
+    return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+           var element = document.createElement('div');
+           element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+                return element;
+       }).catch(error => 'An error occurred while loading the component');
+}
+    // Note that because a network request is involved, some indication
+    // of loading would need to be shown in a production-level site/app.
+   /* button.onclick = e =>import(/!* webpackChunkName: "print" *!/ './print.js').then(module = function () {
+            var print = module.default;
+            print();
+        });*/
+  /*  function imp(q){
+        import q from './print';
+        return  ;
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    }
+// Lodash, now imported by this script
 
     return element;
-}
-
-document.body.appendChild(component());
+}*/
+ getComponent().then(component => {
+     document.body.appendChild(component);
+     });
