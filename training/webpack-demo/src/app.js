6c040915cb213039326e608fb823666866ace0d9
shobhit <shobhitsingh29@gmail.com>
2017-07-19 16:25:53 +0530

    webpack

---
+++ training/webpack-demo/src/app.js
@@ -0,0 +1,62 @@
+window.myTodoApp= {};
+
+myTodoApp.init = function () {
+    var myNodelist = document.getElementsByTagName("LI");
+    for (let i = 0; i < myNodelist.length; i++) {
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        myNodelist[i].appendChild(span);
+    }
+
+// Add a "checked" symbol when clicking on a list item
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function (ev) {
+        if (ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
+        }
+    }, false);
+
+
+// Click on a close button to hide the current list item
+
+};
+myTodoApp.newElement=function(){
+
+
+    var close = document.getElementsByClassName("close");
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+
+// Create a new list item when clicking on the "Add" button
+        var li = document.createElement("li");
+        var inputValue = document.getElementById("myInput").value;
+        var t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        if (inputValue === '') {
+            alert("You must write something!");
+        } else {
+            document.getElementById("myUL").appendChild(li);
+        }
+        document.getElementById("myInput").value = "";
+
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+        for (let i = 0; i < close.length; i++) {
+            close[i].onclick = function () {
+                var div = this.parentElement;
+                div.style.display = "none";
+            }
+        }
+    };
+
+myTodoApp.init();
--- training/webpack-demo/src/index.js
+++ training/webpack-demo/src/index.js
@@ -1,34 +1,38 @@
 /*import _ from 'lodash';*/
 function getComponent() {
-  /*  var element = document.createElement('div');
-    var button = document.createElement('button');
-    var br = document.createElement('br');
-    button.innerHTML = 'Click me and look at the console!';
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');*/
-  /*  element.appendChild(br);
-    element.appendChild(button);*/
-    return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
-           var element = document.createElement('div');
-           element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-                return element;
-       }).catch(error => 'An error occurred while loading the component');
-}
+    /*  var element = document.createElement('div');
+     var button = document.createElement('button');
+     var br = document.createElement('br');
+     button.innerHTML = 'Click me and look at the console!';
+     element.innerHTML = _.join(['Hello', 'webpack'], ' ');*/
+    /*  element.appendChild(br);
+     element.appendChild(button);*/
+    /* return import(/!* webpackChunkName: "lodash" *!/ 'lodash').then(_ => {
+     var element = document.createElement('div');
+     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+     return element;
+     }).catch(error => 'An error occurred while loading the component');
+     }*/
     // Note that because a network request is involved, some indication
-    // of loading would need to be shown in a production-level site/app.
-   /* button.onclick = e =>import(/!* webpackChunkName: "print" *!/ './print.js').then(module = function () {
-            var print = module.default;
-            print();
-        });*/
-  /*  function imp(q){
-        import q from './print';
-        return  ;
+    // onpm startf loading would need to be shown in a production-level site/app.
+    /* button.onclick = e =>import(/!* webpackChunkName: "print" *!/ './print.js').then(module = function () {
+     var print = module.default;
+     print();
+     });*/
+    /*  function imp(q){
+     import q from './print';
+     return  ;
 
 
-    }
-// Lodash, now imported by this script
+     }
+     // Lodash, now imported by this script
 
-    return element;
-}*/
+     return element;
+     }*/
+
+}
+/*
  getComponent().then(component => {
      document.body.appendChild(component);
      });
+*/
