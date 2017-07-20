b229dd9dece9d3cdab9b59315c69a23593834d7d
DESKTOP-H17UMK4\goku <ssingh239@sapient.com>
2017-07-21 00:21:13 +0530

    curry comments aded

--- training/w2d4_setsetting_curry_partial/reduce.js
+++ training/w2d4_setsetting_curry_partial/reduce.js
@@ -1,24 +1,28 @@
-if (!Array.prototype.reduce) {
-    Array.prototype.reduce = function reduce(accumulator) {
-        var i = 0, l = this.length, curr;
-        if (arguments.length < 2) {
+ Array.prototype.reduce = function reduce(accumulator) {
+        debugger;
+        var i = 0, len = this.length, curr; /// here l is the length of the array from which we called it
+        if (arguments.length < 2) { //since value is not pasded here assign current index to  first array value
             curr = this[0];
             i = 1;
         }
         else {
             curr = arguments[1];
         }
-        while (i < l) {
+        while (i < len) {
             if (i in this) {
-                curr = accumulator.call(undefined, curr, this[i], i, this);
-                ++i;
+                curr = accumulator.call(null, curr, this[i]);
+                i++;
             }
         }
         return curr;
     };
-}
+
 var total = [0, 1, 2, 3].reduce(function (sum, value) {
     debugger;
     return sum + value;
 }, 0);
 console.log(total);
+ var flattened = [[0, 1], [2, 3], [4, 5]].reduce(function(a, b) {
+     return a.concat(b);
+ }, []);
+ console.log(flattened);
